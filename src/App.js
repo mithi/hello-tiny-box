3d8a4ce0d7b4c3f8d09b73e4973949ef7b16c500
Mithi <mithi.sevilla@gmail.com>
2020-08-13 19:10:29 +0800

    feat: Add number input field to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
+import NumericInputField from "./NumericInputField"
 
-function App() {
+const ControlBar = () => {
+    const [value, setValue] = useState({ cameraView: 0 })
+
+    const setField = (id, newValue) => {
+        if (id === "cameraView") {
+            setValue({ cameraView: newValue })
+        }
+        console.log(value, id, newValue)
+    }
+
     return (
-        <div id="main">
-            <div id="controls">Side</div>
-            <div id="plot">Plot goes here</div>
+        <div id="controls">
+            <NumericInputField
+                label
+                id="cameraView"
+                value={value.cameraView}
+                setField={setField}
+                rangeParams={{ minVal: -100, maxVal: 100, stepVal: 5 }}
+            />
         </div>
     )
+}
+
+const Plot = () => <div id="plot">Plot goes here</div>
+
+class App extends React.Component {
+    render() {
+        return (
+            <div id="main">
+                <ControlBar />
+                <Plot />
+            </div>
+        )
+    }
 }
 
 export default App
