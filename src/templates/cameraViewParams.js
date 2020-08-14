389b5ca8908ae77abf03aa95ef564cfc21446546
Mithi <mithi.sevilla@gmail.com>
2020-08-14 17:38:05 +0800

    Add template for camera view and cube data structures

---
+++ src/templates/cameraViewParams.js
@@ -0,0 +1,88 @@
+const INIT_STATE = {
+    rx: 0,
+    ry: 0,
+    rz: 0,
+    tx: 0,
+    ty: 0,
+    tz: 0,
+    zoom: 10,
+}
+
+/** THE PROPS REQUIRED FOR EACH INPUT FIELD **/
+const STATE_PROPS = {
+    rx: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotX",
+        id: "camera-view-rot-x",
+    },
+    ry: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotY",
+        id: "camera-view-rot-z",
+    },
+    rz: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotZ",
+        id: "camera-view-rot-y",
+    },
+    tx: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.X",
+        id: "camera-view-trans-x",
+    },
+    ty: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.Y",
+        id: "camera-view-trans-y",
+    },
+    tz: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.Z",
+        id: "camera-view-trans-z",
+    },
+    zoom: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "zoom",
+        id: "camera-view-zoom",
+    },
+}
+
+const ID_TO_KEY_MAP = {
+    [STATE_PROPS.rx.id]: "rx",
+    [STATE_PROPS.ry.id]: "ry",
+    [STATE_PROPS.rz.id]: "rz",
+    [STATE_PROPS.tx.id]: "tx",
+    [STATE_PROPS.ty.id]: "ty",
+    [STATE_PROPS.tz.id]: "tz",
+    [STATE_PROPS.zoom.id]: "zoom",
+}
+
+export default { INIT_STATE, STATE_PROPS, ID_TO_KEY_MAP }
---
+++ src/templates/cubeStateParams.js
@@ -0,0 +1,128 @@
+const INIT_STATE = {
+    rx: 0,
+    ry: 0,
+    rz: 0,
+    tx: 0,
+    ty: 0,
+    tz: 0,
+    sx: 1,
+    sy: 1,
+    sz: 1,
+    color: 0,
+}
+
+/** THE PROPS REQUIRED FOR EACH INPUT FIELD **/
+const STATE_PROPS = {
+    rx: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotX",
+        id: "cube-state-rot-x",
+    },
+    ry: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotY",
+        id: "cube-state-rot-z",
+    },
+    rz: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "rotZ",
+        id: "cube-state-rot-y",
+    },
+
+    // ---------------------------
+
+    tx: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.X",
+        id: "cube-state-trans-x",
+    },
+    ty: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.Y",
+        id: "cube-state-trans-y",
+    },
+    tz: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "t.Z",
+        id: "cube-state-trans-z",
+    },
+
+    // ---------------------------
+
+    sx: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "s.X",
+        id: "cube-state-scale-x",
+    },
+
+    sy: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "s.Y",
+        id: "cube-state-scale-y",
+    },
+    sz: {
+        rangeParams: {
+            minVal: -90,
+            maxVal: 90,
+            stepVal: 0.5,
+        },
+        label: "s.Z",
+        id: "cube-state-scale-z",
+    },
+    // ---------------------------
+
+    color: {
+        label: "color",
+        id: "cube-color",
+    },
+}
+
+const ID_TO_KEY_MAP = {
+    [STATE_PROPS.rx.id]: "rx",
+    [STATE_PROPS.ry.id]: "ry",
+    [STATE_PROPS.rz.id]: "rz",
+
+    [STATE_PROPS.tx.id]: "tx",
+    [STATE_PROPS.ty.id]: "ty",
+    [STATE_PROPS.tz.id]: "tz",
+
+    [STATE_PROPS.sx.id]: "sx",
+    [STATE_PROPS.sy.id]: "sy",
+    [STATE_PROPS.sz.id]: "sz",
+
+    [STATE_PROPS.color.id]: "color",
+}
+
+export default { INIT_STATE, STATE_PROPS, ID_TO_KEY_MAP }
