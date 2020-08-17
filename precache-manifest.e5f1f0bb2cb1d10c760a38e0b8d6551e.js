9eb862b4a94073753ee29231e2e97ac754091a29
Mithi <mithi.sevilla@gmail.com>
2020-08-18 02:10:09 +0800

    Updates

---
+++ precache-manifest.e5f1f0bb2cb1d10c760a38e0b8d6551e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852de043ad1bdb91db52c44fc229e9e6",
+    "url": "/hello-tiny-box/index.html"
+  },
+  {
+    "revision": "fcd85b50f3f29c842d50",
+    "url": "/hello-tiny-box/static/js/2.8dd767be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hello-tiny-box/static/js/2.8dd767be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f365e9d1a5b37d2c71",
+    "url": "/hello-tiny-box/static/js/main.02b843be.chunk.js"
+  },
+  {
+    "revision": "b1ddef5152d3e4b60411",
+    "url": "/hello-tiny-box/static/js/runtime-main.7c07eca0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hello-tiny-box/precache-manifest.e5f1f0bb2cb1d10c760a38e0b8d6551e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello-tiny-box/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
